e7c80f606bb67a8238b4dc74220a68f346c98107
Jacó Apolinário <jacoapollinario@gmail.com>
2021-07-01 21:36:54 -0300

    Add features

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userFound = this.usersRepository.findById(user_id);
+
+    if (!userFound) {
+      throw new Error("User not exists!");
+    }
+
+    const userTurnAdmin = this.usersRepository.turnAdmin(userFound);
+
+    return userTurnAdmin;
   }
 }
 
